6b51d8cbacd038997ab5aea606a264287a47108b
hellarose <57387848+hellarose@users.noreply.github.com>
2020-07-07 11:53:53 +0800

    test vue

---
+++ usfive/src/components/fortest.vue
@@ -0,0 +1,47 @@
+
+<style scoped>
+
+</style>
+
+
+<template>
+    <div>
+        for test
+    </div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+    created(){
+
+    },
+    mounted(){
+
+    },
+    //props:{},//[]
+    methods:{
+
+    },
+    computed:{
+
+    },
+    watch:{
+
+    },
+    filters:{
+
+    },
+    directives:{
+
+    },
+    components:{
+
+    },
+};
+</script>
