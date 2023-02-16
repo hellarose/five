43134c0473b97788bb5c4f07a41054313d434336
hellarose <1812003189@qq.com>
2023-02-16 14:43:00 +0800

    应用SVG icon

--- src/components/footbar.vue
+++ src/components/footbar.vue
@@ -7,9 +7,27 @@
   <div>
     <van-tabbar route>
       <van-tabbar-item replace to="/" icon="home-o">首页</van-tabbar-item>
-      <van-tabbar-item replace to="/ins" icon="search">课程</van-tabbar-item>
-      <van-tabbar-item replace to="/ins" icon="search">约课记录</van-tabbar-item>
-      <van-tabbar-item replace to="/ins" icon="search">练习</van-tabbar-item>
+      <van-tabbar-item replace to="/mylesson">
+        <img
+          slot="icon"
+          slot-scope="scope"
+          width="16"
+          :src="
+            scope.active ? '/static/course-active.svg' : '/static/course.svg'
+          "
+        />
+        <span>课程</span>
+      </van-tabbar-item>
+      <van-tabbar-item replace to="/mymoney">
+        <img
+          slot="icon"
+          slot-scope="scope"
+          width="16"
+          :src="scope.active ? '/static/order-active.svg' : '/static/order.svg'"
+        />
+        <span>约课记录</span>
+      </van-tabbar-item>
+      <van-tabbar-item replace to="/feedback" icon="search">练习</van-tabbar-item>
       <van-tabbar-item replace to="/self" icon="friends">我的</van-tabbar-item>
     </van-tabbar>
   </div>
@@ -29,6 +47,6 @@ export default {
   watch: {},
   filters: {},
   directives: {},
-  components: {}
+  components: {},
 };
 </script>
